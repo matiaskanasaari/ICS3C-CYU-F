804660efd879e4b43be1e3d47190bda32e49d8d5
matiaskanasaari <145567942+matiaskanasaari@users.noreply.github.com>
2023-11-28 09:19:59 -0500

    resub

--- cone.py
+++ cone.py
@@ -2,4 +2,14 @@
 # Description: 
 
 from math import pi
+from math import hypot
+
+def get_cone_volume (radius,height):
+    volume = (pi * radius ** 2 * height) / 3
+    return volume
+
+def get_surface_area (radius, height):
+    slant_height = hypot(radius,height)
+    area = pi * radius ** 2 + pi * radius * slant_height
+    return area
 
