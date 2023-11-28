fe0daa65ff32750922155d7a4c4c6252c8891aa2
matiaskanasaari <145567942+matiaskanasaari@users.noreply.github.com>
2023-11-28 08:45:23 -0500

    Update grade.py

--- grade.py
+++ grade.py
@@ -1,16 +1,16 @@
 # Programmer: 
 # Description: 
 
-def convert_grade (
-if grade < 0 or grade > 100:
+def convert_grade (grade):
+    if grade < 0 or grade > 100:
             return("Invalid percent!")
-elif grade >=80:
-            return(f"{grade}% is a Level A.")
-elif grade >=70:
-            return(f"{grade}% is a Level B.")
-elif grade >=60:
-            return(f"{grade}% is a Level C.")
-elif grade >=50:
-            return(f"{grade}% is a Level D.")
-elif 0 <= grade <= 50:
-            return(f"{grade}% is an F. ")
+    elif grade >=80:
+            return "A"
+    elif grade >=70:
+            return "B"
+    elif grade >=60:
+            return "C"
+    elif grade >=50:
+            return "D"
+    elif 0 <= grade <= 50:
+            return "F"
